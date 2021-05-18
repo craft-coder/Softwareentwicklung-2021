a8b9172335f147a904670f465e196e4a56750bcc
Dr. Marius Feilhauer <mail@mariusfeilhauer.de>
2021-05-18 22:26:01 +0200

    Added image output

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,31 @@
-#include <iostream>
 #include "Vec3.h"
+#include <iostream>
+#include <thread>
 
 int main() {
 
-    raytracer::Vec3 v1{};
-    std::cout << "the content of v1 is: " << v1.getX() << "," << v1.getY() << "," << v1.getZ() << std::endl;
-    
-    raytracer::Vec3 v2(1.0, 2.0, 3.0);
-    std::cout << "the content of v2 is: " << v2.getX() << "," << v2.getY() << "," << v2.getZ() << std::endl;
+    // Image
+    const int imageWidth = 256;
+    const int imageHeigth = 256;
+
+    // Render
+    std::cout << "P3\n" << imageWidth << ' ' << imageHeigth << "\n255\n";
+
+    for (int j = imageHeigth - 1; j >= 0; --j) {
+        std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
+        for (int i = 0; i < imageWidth; ++i) {
+            auto r = double(i) / (imageWidth - 1);
+            auto g = double(j) / (imageHeigth - 1);
+            auto b = 0.25;
+
+            int ir = static_cast<int>(255.999 * r);
+            int ig = static_cast<int>(255.999 * g);
+            int ib = static_cast<int>(255.999 * b);
+
+            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+        }
+    }
+    std::cerr << "\nDone.\n";
 
     return 0;
 }
