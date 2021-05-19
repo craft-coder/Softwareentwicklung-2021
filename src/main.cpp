fa43cef1fd189ab7ed5cf9afdfe60ead3ba64d05
Dr. Marius Feilhauer <mail@mariusfeilhauer.de>
2021-05-19 23:33:16 +0200

    Updated main as done in lecture

--- src/main.cpp
+++ src/main.cpp
@@ -4,16 +4,6 @@
 
 using namespace raytracer;
 
-void writeColor(std::ostream& out, Color color) {
-    
-    // Write the translated [0,255] value of each Color component.
-    auto red = static_cast<int>(255.999 * color.x());
-    auto green = static_cast<int>(255.999 * color.y());
-    auto blue = static_cast<int>(255.999 * color.z());
-    
-    out << red << ' ' << green << ' ' << blue << '\n';
-}
-
 int main() {
 
     // Image
@@ -21,17 +11,26 @@ int main() {
     const int imageHeigth = 256;
 
     // Render
+    std::cerr << "Starting rendering" << std::endl;
     std::cout << "P3\n" << imageWidth << ' ' << imageHeigth << "\n255\n";
 
     for (int j = imageHeigth - 1; j >= 0; --j) {
         std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
+       
         for (int i = 0; i < imageWidth; ++i) {
             
-            auto color = Color(double(i) / (imageWidth - 1), double(j) / (imageHeigth - 1), 0.25);
-            writeColor(std::cout, color);
+            auto color = Color(double(i) / (imageWidth - 1), 1.0, 1.0);
             
+            // Write the translated [0,255] value of each Color component.
+            auto red = static_cast<int>(255.999 * color.x());
+            auto green = static_cast<int>(255.999 * color.y());
+            auto blue = static_cast<int>(255.999 * color.z());
+            
+            std::cout << red << ' ' << green << ' ' << blue << '\n';
         }
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
+
     std::cerr << "\nDone.\n";
 
     return 0;
